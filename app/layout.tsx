0ca64a7a112306e905c9a0f52b3291a31c0b1916
Ahmed Mohamed <105979851+AhmedMohamed809@users.noreply.github.com>
2023-09-01 22:16:25 +0100

    add Image and Image config

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
 import Header from '@/components/Header'
+import Intro from '@/components/Intro'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
--- components/Intro.tsx
+++ components/Intro.tsx
@@ -1,8 +1,16 @@
 import React from 'react'
-
+import Image from "next/image"
 function Intro() {
   return (
-    <div>Intro</div>
+    <section>
+        <div className='flex items-center justify-center'>
+            <div>
+                <Image src="https://media.licdn.com/dms/image/C4E03AQEr5LIkejyb-Q/profile-displayphoto-shrink_800_800/0/1655157555401?e=1698883200&v=beta&t=NshqrTf7GAgdcLFr4EcwjoPQshERiesW2B_r-Hk0ZEc"
+                 alt="my_photo" width="192" height="192" quality="95" priority={true} />
+            </div>
+
+        </div>
+    </section>
   )
 }
 
