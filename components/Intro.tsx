0360f4f8dead527f6510c1c89f68f9e9dabf6fbd
Ahmed Mohamed <105979851+AhmedMohamed809@users.noreply.github.com>
2023-09-04 12:49:16 +0100

    add motion

--- components/Intro.tsx
+++ components/Intro.tsx
@@ -39,12 +39,15 @@ function Intro() {
         initial={{ opacity: 0, y: 100 }}
         animate={{ opacity: 1, y: 0 }}
       >
-        <p className='mb-10 mt-4 px-4 text-2xl font-medium leading-[1.5] sm:text-4xl'>
+        <motion.p className='mb-10 mt-4 px-4 text-2xl font-medium leading-[1.5] sm:text-4xl'
+        initial={{ opacity: 0, y: 100 }}
+        animate={{ opacity: 1, y: 0 }}
+        >
         <span className="font-bold">Hello, I'm Ahmed Mohamed.</span> I'm a{" "}
         <span className="font-bold">a junior full-stack developer</span> I enjoy
         building <span className="italic">sites</span>. My focus is{" "}
         <span className="underline">React (Next.js)</span>.
-        </p>
+        </motion.p>
       </motion.h1>
     </section>
   )
