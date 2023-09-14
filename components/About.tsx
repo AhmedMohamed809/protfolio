0a654db1690948bed2b6ddab7888c406e0df8e38
Ahmed Mohamed <105979851+AhmedMohamed809@users.noreply.github.com>
2023-09-14 23:47:27 +0100

    add about me

--- components/About.tsx
+++ components/About.tsx
@@ -14,7 +14,7 @@ function About() {
      <SectionHeading>About me</SectionHeading>
      <p className="mb-3">
         After graduating with a degree in{" "}
-        <span className="font-medium">Accounting</span>, I decided to pursue my
+        <span className="font-medium">Computer Science</span>, I decided to pursue my
         passion for programming. I enrolled in a coding bootcamp and learned{" "}
         <span className="font-medium">full-stack web development</span>.{" "}
         <span className="italic">My favorite part of programming</span> is the
@@ -22,7 +22,7 @@ function About() {
         feeling of finally figuring out a solution to a problem. My core stack
         is{" "}
         <span className="font-medium">
-          React, Next.js, Node.js, and MongoDB
+          React, Next.js, Node.js, and PSQL
         </span>
         . I am also familiar with TypeScript and Prisma. I am always looking to
         learn new technologies. I am currently looking for a{" "}
