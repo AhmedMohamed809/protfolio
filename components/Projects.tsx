bb5d9847411cfb4eb2ced9b78f3942cebeb12396
Ahmed Mohamed <105979851+AhmedMohamed809@users.noreply.github.com>
2023-09-19 14:33:02 +0100

    add projects div

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -1,12 +1,35 @@
 import React from 'react'
 import SectionHeading from './SectionHeading'
+import { projectsData } from '@/lib/data'
+import { type } from 'os'
 
 function Projects() {
   return (
     <section>
         <SectionHeading>My Projects</SectionHeading>
+        <div>
+
+            {projectsData.map((project,index)=>(
+                <React.Fragment key={index}>
+                <Project {...project}/>
+                </React.Fragment>
+
+            ))}
+        </div>
     </section>
   )
 }
 
+type ProjectProps = (typeof projectsData)[number];
+function Project({
+    title,
+    description,
+    tags,
+    imageUrl,
+}: ProjectProps){
+    return <article>
+
+        
+    </article>
+}
 export default Projects
