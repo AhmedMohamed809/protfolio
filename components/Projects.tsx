92c4cc4a55abb157884c0855ea1f28999c9ffc4a
Ahmed Mohamed <105979851+AhmedMohamed809@users.noreply.github.com>
2023-09-19 14:48:41 +0100

    add style to UL

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -31,9 +31,10 @@ function Project({
     return <section className='bg-gray-100 max-w-[42rem] border border-black/5 overflow-hidden sm:pr-8'>
         <h3 className=' text-2xl font-semibold'>{title}</h3>
         <p className=' mt-2 leading-relaxed text-gray-700'>{description}</p>
-        <ul>
+        <ul className=' flex flex-wrap mt-4 gap-2'>
             {tags.map((tag,index)=>(
                 <li key={index} className=' bg-black/[0.7] px-3 py-1 text-[0.7rem] uppercase tracking-wide text-white rounded-full
+
                 '>{tag}</li>
             ))}
         </ul>
