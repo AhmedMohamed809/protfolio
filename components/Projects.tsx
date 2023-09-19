138751c0c9c871c352f6b3c151f5ea8e05ffab1a
Ahmed Mohamed <105979851+AhmedMohamed809@users.noreply.github.com>
2023-09-19 14:41:32 +0100

    add style to section and h3

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import SectionHeading from './SectionHeading'
 import { projectsData } from '@/lib/data'
 import { type } from 'os'
+import Image from 'next/image'
 
 function Projects() {
   return (
@@ -27,9 +28,17 @@ function Project({
     tags,
     imageUrl,
 }: ProjectProps){
-    return <article>
+    return <section className='bg-gray-100 max-w-[42rem] border border-black/5 overflow-hidden sm:pr-8'>
+        <h3 className=' text-2xl font-semibold'>{title}</h3>
+        <p>{description}</p>
+        <ul>
+            {tags.map((tag,index)=>(
+                <li key={index}>{tag}</li>
+            ))}
+        </ul>
+        <Image src={imageUrl} alt={title} quality={95} />
 
-        
-    </article>
+
+    </section>
 }
 export default Projects
