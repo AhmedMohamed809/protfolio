a3dc14ea14a34a1730aafc54e245db14a493746d
Ahmed Mohamed <105979851+AhmedMohamed809@users.noreply.github.com>
2023-09-19 14:46:35 +0100

    add style to list

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -30,10 +30,11 @@ function Project({
 }: ProjectProps){
     return <section className='bg-gray-100 max-w-[42rem] border border-black/5 overflow-hidden sm:pr-8'>
         <h3 className=' text-2xl font-semibold'>{title}</h3>
-        <p>{description}</p>
+        <p className=' mt-2 leading-relaxed text-gray-700'>{description}</p>
         <ul>
             {tags.map((tag,index)=>(
-                <li key={index}>{tag}</li>
+                <li key={index} className=' bg-black/[0.7] px-3 py-1 text-[0.7rem] uppercase tracking-wide text-white rounded-full
+                '>{tag}</li>
             ))}
         </ul>
         <Image src={imageUrl} alt={title} quality={95} />
