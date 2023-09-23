40adb05c29f92bfea1f2911098b496c790cbc147
Ahmed Mohamed <105979851+AhmedMohamed809@users.noreply.github.com>
2023-09-23 17:16:33 +0100

    add style to image and div

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -28,17 +28,19 @@ function Project({
     tags,
     imageUrl,
 }: ProjectProps){
-    return <section className='bg-gray-100 max-w-[42rem] border border-black/5 overflow-hidden sm:pr-8'>
+    return <section className='bg-gray-100 max-w-[42rem] border border-black/5 overflow-hidden sm:pr-8 relative sm:h-[20rem]'>
+        <div className=' py-4 px-5 sm:pl-10 sm:pr-2 sm:pt-10 sm:max-w-[50%] flex flex-col'>
         <h3 className=' text-2xl font-semibold'>{title}</h3>
         <p className=' mt-2 leading-relaxed text-gray-700'>{description}</p>
-        <ul className=' flex flex-wrap mt-4 gap-2'>
+        <ul className=' flex flex-wrap mt-4 gap-2 mt-auto'>
             {tags.map((tag,index)=>(
                 <li key={index} className=' bg-black/[0.7] px-3 py-1 text-[0.7rem] uppercase tracking-wide text-white rounded-full
 
                 '>{tag}</li>
             ))}
         </ul>
-        <Image src={imageUrl} alt={title} quality={95} className=' absolute' />
+        </div>
+        <Image src={imageUrl} alt={title} quality={95} className=' absolute top-8 -right-40 w-[28.25rem]  rounded-top-lg shadow-2xl' />
 
 
     </section>
