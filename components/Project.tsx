d75d84d436a29186b9805f5b0c2e239f79a8ad9a
Ahmed Mohamed <105979851+AhmedMohamed809@users.noreply.github.com>
2023-09-26 22:25:30 +0100

    maping url

--- components/Project.tsx
+++ components/Project.tsx
@@ -9,6 +9,7 @@ export default function Project({
     title,
     description,
     tags,
+    url,
     imageUrl,
 }: ProjectProps){
     const ref = useRef<HTMLDivElement>(null);
@@ -30,7 +31,7 @@ export default function Project({
         <div className=' pt-4 pb-7 px-5 sm:pl-10 sm:pr-2 sm:pt-10 sm:max-w-[50%]
          flex flex-col h-full group-even:ml-[18rem]'>
         <h3 className=' text-2xl font-semibold'>{title}</h3>
-        <p className=' mt-2 leading-relaxed text-gray-700'>{description}</p>
+        <p className=' mt-2 leading-relaxed text-gray-700'> <a href={url}>{description}</a></p>
         <ul className=' flex flex-wrap mt-4 gap-2 sm:mt-auto'>
             {tags.map((tag,index)=>(
                 <li key={index} className=' bg-black/[0.7] px-3 py-1 text-[0.7rem] uppercase tracking-wide text-white rounded-full
@@ -39,7 +40,7 @@ export default function Project({
             ))}
         </ul>
         </div>
-        <Image src={imageUrl} alt={title} quality={95} className=' absolute top-8 -right-40 w-[28.25rem] 
+        <Image src={imageUrl}  alt={title} quality={95} className=' absolute top-8 -right-40 w-[28.25rem] 
          rounded-top-lg shadow-2xl group-even:right-[initial] group-even:-left-40
          group-hover:-translate-x-3
          group-hover:translate-y-3
