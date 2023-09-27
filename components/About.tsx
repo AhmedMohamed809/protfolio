18d4b9f82777f9bf35d94f9158075dfbc3fac8e5
Ahmed Mohamed <105979851+AhmedMohamed809@users.noreply.github.com>
2023-09-27 21:11:41 +0100

    add more about me

--- components/About.tsx
+++ components/About.tsx
@@ -26,16 +26,15 @@ function About() {
         </span>
         . I am also familiar with TypeScript and Prisma. I am always looking to
         learn new technologies. I am currently looking for a{" "}
-        <span className="font-medium">full-time position</span> as a software
+        <span className="font-medium">part-time position</span> as a junior software
         developer.
       </p>
 
       <p>
-        <span className="italic">When I'm not coding</span>, I enjoy playing
-        video games, watching movies, and playing with my dog. I also enjoy{" "}
+        <span className="italic">When I'm not coding</span>, I enjoy take some pictures , go to the Gym. I also enjoy{" "}
         <span className="font-medium">learning new things</span>. I am currently
         learning about{" "}
-        <span className="font-medium">history and philosophy</span>. I'm also
+        <span className="font-medium">Clean code book</span>. I'm also
         learning how to play the guitar.
       </p>
     </motion.section>
