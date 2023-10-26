f5e19f67c47ed71627ed9ea64a6220e886974633
Ahmed Mohamed <105979851+AhmedMohamed809@users.noreply.github.com>
2023-10-26 20:45:31 +0100

    create skills pag

---
+++ components/Skills.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import SectionHeading from './SectionHeading'
+
+function Skills() {
+  return (
+    <section>
+      <SectionHeading>My Skills</SectionHeading>
+    </section>
+  )
+}
+
+export default Skills
