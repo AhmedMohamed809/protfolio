e20ffa831ae5ee88181d16ea1756a4dcfe960224
Ahmed Mohamed <105979851+AhmedMohamed809@users.noreply.github.com>
2023-10-30 07:26:39 +0000

    add style to download button

--- components/Intro.tsx
+++ components/Intro.tsx
@@ -60,7 +60,7 @@ function Intro() {
        <Link href="mailto:Ahmedabdulalgane@gmail.com"  target='_blank' className='group bg-gray-900 text-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105 transition
         
         ' > Contact me here <BsArrowRight className='opacity-70 group-hover:translate-x-1 transition'/></Link>
-        <a href="/Ahmed_CV.pdf" download  className='group bg-gray-900 text-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105 transition cursor-pointer' >Download CV <HiDownload className='opacity-60 group-hover:translate-y-1 transition'/></a>
+        <a href="/Ahmed_CV.pdf" download  className='group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10' >Download CV <HiDownload className='opacity-60 group-hover:translate-y-1 transition'/></a>
         <a href="https://www.linkedin.com/in/ahmed-mohamed-193005242/" target='_blank' className='bg-white   p-4 text-gray-700 flex items-center gap-2 rounded-full border border-black/10 outline-none focus:scale-[1.15] hover:text-gray-950 hover:scale-[1.15] active:scale-105 transition cursor-pointer'> <BsLinkedin/></a>
         <a href="https://github.com/AhmedMohamed809" target='_blank' className='bg-white   p-4 text-gray-700 flex text-[1.35rem] items-center gap-2 rounded-full outline-none focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition cursor-pointer'> <FaGithubSquare/></a>
 
