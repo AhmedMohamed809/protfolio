aa7924b39ce69354c2bc149cfb4f8cb5da4dcd46
Ahmed Mohamed <105979851+AhmedMohamed809@users.noreply.github.com>
2023-11-15 07:09:33 +0000

    change p tag to div

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -7,11 +7,11 @@ function Footer() {
     <small className="mb-2 block text-xs">
       &copy; 2023 Ahmed Mohamed. All rights reserved.
     </small>
-    <p className="text-xs">
+    <div className="text-xs">
       <span className="font-semibold">About this website:</span> built with
       React & Next.js (App Router & Server Actions), TypeScript, Tailwind CSS,
       Framer Motion, React Email & Resend, Vercel hosting.
-    </p>
+    </div>
   </footer>
   )
 }
